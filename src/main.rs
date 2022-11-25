97f7ca913b3a7510e18ebab4f278eec61421b711
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2022-11-25 17:13:56 -0600

    crate ash instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use ash::version::EntryV1_0;
+use ash::version::InstanceV1_0;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let entry = unsafe { ash::Entry::new()? };
+    let instance = unsafe { entry.create_instance(&Default::default(), None)? };
+
+    unsafe { instance.destroy_instance(None) };
+    Ok(())
 }
